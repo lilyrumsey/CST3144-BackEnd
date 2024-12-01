056dd49ea51758ff9d784bf79189a44ad6fd1a42
Lily Rumsey <lr604@live.mdx.ac.uk>
2024-12-01 21:59:24 +0000

    CORS Middleware Addition

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const morgan = require('morgan');
 const winston = require('winston');
 const app = express();
+
+// CORS Middleware (to handle CORS issues)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+    next();
+  });
+
+  
